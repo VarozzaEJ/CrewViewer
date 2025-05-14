dfd7b80a8dfc5c144f2b7c6494df75b362f5a35e
Evan Varozza <VarozzaEJ@gmail.com>
2025-05-14 13:58:09 -0600

    Crew Pane finished

--- src/main/java/javafxexample/CrewPane.java
+++ src/main/java/javafxexample/CrewPane.java
@@ -1,7 +1,42 @@
 package javafxexample;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class CrewPane extends FlowPane {
-    
+    private Character[] crew;
+    public CrewPane(Character[] crew) {
+        this.crew = crew;
+        setStyle("-fx-background-color: midnightblue");
+        setHgap(25);
+        setVgap(25);
+        setAlignment(Pos.CENTER);
+        Label titleLabel = new Label("USS Unrelianat - Crew");
+        titleLabel.setFont(new Font("Helvetica", 35));
+        titleLabel.setTextFill(Color.GOLD);
+        getChildren().add(titleLabel);
+        for (Character character : crew) {
+            CharacterSprite sprite = new CharacterSprite(character);
+            sprite.setOnMouseClicked(this::mouseClickSprite);
+            getChildren().add(sprite);
+        }
+    }
+
+    private void mouseClickSprite(MouseEvent event) {
+        CharacterSprite source = (CharacterSprite)event.getSource();
+        source.toggleState();
+        if(crew[0].getNumberAlive() == 0) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setContentText("The Entire Crew Has Died!");
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }
+    }
 }
